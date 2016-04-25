51ec9b2b719296eed95212f63dc780fb8e6f7466
Filip Stamenkovic <ficadub@gmail.com>
2016-04-25 23:18:13 +0200

    First commit

---
+++ app/src/main/java/nos/elfak/rs/senseclient/MainActivity.java
@@ -0,0 +1,13 @@
+package nos.elfak.rs.senseclient;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
