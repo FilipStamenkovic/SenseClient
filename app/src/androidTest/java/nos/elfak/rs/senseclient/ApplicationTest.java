12ef696ce37930a427e7a64aed0ae549fdbf84ed
Filip Stamenkovic <ficadub@gmail.com>
2016-04-26 20:54:41 +0200

    Third one

---
+++ app/src/androidTest/java/nos/elfak/rs/senseclient/ApplicationTest.java
@@ -0,0 +1,13 @@
+package nos.elfak.rs.senseclient;
+
+import android.app.Application;
+import android.test.ApplicationTestCase;
+
+/**
+ * <a href="http://d.android.com/tools/testing/testing_android.html">Testing Fundamentals</a>
+ */
+public class ApplicationTest extends ApplicationTestCase<Application> {
+    public ApplicationTest() {
+        super(Application.class);
+    }
+}
